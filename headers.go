4fa1db225db83a3df9f3750752711c9af703b8bd
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2021-11-26 14:42:38 +0700

    Refactor request code

---
+++ headers.go
@@ -0,0 +1,18 @@
+package gostman
+
+import "net/http"
+
+// Headers sets request headers.
+func (r *Request) Headers(f func(http.Header)) {
+	f(r.headers)
+}
+
+func recomendedHeader() http.Header {
+	headers := make(http.Header)
+	headers.Add("Host", "<calculated when request is sent>")
+	headers.Add("User-Agent", "Gostman/"+gostmanVersion)
+	headers.Add("Accept", "*/*")
+	headers.Add("Accept-Encoding", "gzip, deflate, br")
+	headers.Add("Connection", "keep-alive")
+	return headers
+}
